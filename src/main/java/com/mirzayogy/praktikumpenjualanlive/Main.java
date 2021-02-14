31f1a9503d099936600be94519774cc2ac20de26
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2021-02-14 14:23:06 +0800

    testing read data

--- src/main/java/com/mirzayogy/praktikumpenjualanlive/Main.java
+++ src/main/java/com/mirzayogy/praktikumpenjualanlive/Main.java
@@ -3,6 +3,7 @@ package com.mirzayogy.praktikumpenjualanlive;
 import com.mirzayogy.praktikumpenjualanlive.db.Database;
 import com.mirzayogy.praktikumpenjualanlive.model.JenisBarang;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +13,11 @@ public class Main {
         Connection con = db.getConnection();
         
         JenisBarang jenisBarang = new JenisBarang(con);
-        jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
-        jenisBarang.create();
+//        jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
+//        jenisBarang.create();
+
+        ArrayList<Object> list = jenisBarang.read();
+        JenisBarang jb = (JenisBarang) list.get(1);
+        System.out.println(jb.getNamaJenisBarang());
     }
 }
