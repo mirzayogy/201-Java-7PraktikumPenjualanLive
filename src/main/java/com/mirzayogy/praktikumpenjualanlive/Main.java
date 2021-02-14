a13513d8be87b9525d0fce1e27c71a1b4c0c1188
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2021-02-14 13:49:36 +0800

    Uji koneksi ke database

--- src/main/java/com/mirzayogy/praktikumpenjualanlive/Main.java
+++ src/main/java/com/mirzayogy/praktikumpenjualanlive/Main.java
@@ -1,6 +1,13 @@
 package com.mirzayogy.praktikumpenjualanlive;
+
+import com.mirzayogy.praktikumpenjualanlive.db.Database;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        
+        Database db = new Database();
+        Connection con = db.getConnection();
     }
 }
---
+++ src/main/java/com/mirzayogy/praktikumpenjualanlive/db/Database.java
@@ -0,0 +1,29 @@
+package com.mirzayogy.praktikumpenjualanlive.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Database {
+    
+    private final String URL = "jdbc:mysql://localhost:3306/";
+    private final String DB_NAME = "praktikum_penjualan";
+    private final String USER = "root";
+    private final String PASS = "";
+    
+    public Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Koneksi Berhasil");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        
+        return con;
+    }
+    
+}
