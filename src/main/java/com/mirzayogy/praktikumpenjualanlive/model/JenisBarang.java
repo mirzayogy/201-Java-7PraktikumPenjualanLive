8884b30e52c8ffbbf00040a4eea4eb636739e101
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2021-02-14 13:57:10 +0800

    mengisi atribut

---
+++ src/main/java/com/mirzayogy/praktikumpenjualanlive/model/JenisBarang.java
@@ -0,0 +1,15 @@
+package com.mirzayogy.praktikumpenjualanlive.model;
+
+import java.sql.Connection;
+
+
+public class JenisBarang {
+
+    Connection con;
+
+    
+    private int id;
+    private String namaJenisBarang;
+
+
+}
